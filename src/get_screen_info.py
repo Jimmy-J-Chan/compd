fbb30186802834ce2154bfb7f8719847b6f4427b
J <jimmychan0507@gmail.com>
2026-01-13 17:44:49 +1000

    test screen size

--- src/get_screen_info.py
+++ src/get_screen_info.py
@@ -7,8 +7,11 @@ def get_client_screen_data(key=''):
     dim_screen['screen_width'] = streamlit_js_eval(js_expressions='screen.width', key=f"w_{key}")
     dim_screen['screen_height'] = streamlit_js_eval(js_expressions='screen.height', key=f"h_{key}")
 
-    dim_screen['screen_iwidth'] = streamlit_js_eval(js_expressions='window.innerWidth', key=f"iw_{key}")
-    dim_screen['screen_iheight'] = streamlit_js_eval(js_expressions='window.innerHeight', key=f"ih_{key}")
+    dim_screen['win_iwidth'] = streamlit_js_eval(js_expressions='window.innerWidth', key=f"iw_{key}")
+    dim_screen['win_iheight'] = streamlit_js_eval(js_expressions='window.innerHeight', key=f"ih_{key}")
+
+    dim_screen['win_owidth'] = streamlit_js_eval(js_expressions='window.outerWidth', key=f"iw_{key}")
+    dim_screen['win_oheight'] = streamlit_js_eval(js_expressions='window.outerHeight', key=f"ih_{key}")
     return dim_screen
 
 if __name__ == '__main__':
