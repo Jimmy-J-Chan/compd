6365a38e8e1d9de63a49139b0f2234690122bf2f
J <jimmychan0507@gmail.com>
2026-01-14 14:50:55 +1000

    add page logo

--- compd.py
+++ compd.py
@@ -8,8 +8,9 @@ st.set_page_config(page_title="Compd",
                    #layout='wide',
                    layout="centered",
                    initial_sidebar_state='expanded',
-                   page_icon='./logo/compd_logo_white.png',
+                   #page_icon='./logo/compd_logo_white.png',
                    #page_icon='./logo/icon_compd_logo_clear.png',
+                   page_icon=':chart_with_upwards_trend:'
                    )
 
 
