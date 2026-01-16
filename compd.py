9f09aeebf9cafa729ad38a5814aaf89b7767e24d
J <jimmychan0507@gmail.com>
2026-01-16 16:23:38 +1000

    compd mobile

--- compd.py
+++ compd.py
@@ -3,11 +3,11 @@ from src.manage_screen_res import get_screen_width
 from compd_desktop import compd_desktop
 from compd_mobile import compd_mobile
 
-ALWAYS_USE_DESKTOP = True
+ALWAYS_USE_DESKTOP = False
 
 
 if __name__ == '__main__':
-    screen_width = get_screen_width()
+    screen_width = get_screen_width() # TODO: remove empty space
     if screen_width is not None:
         if (screen_width<1000) & (not ALWAYS_USE_DESKTOP):
             compd_mobile()
