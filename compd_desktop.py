d3cff23f42a380e49be0c9c22b87504764429522
J <jimmychan0507@gmail.com>
2026-01-27 18:51:58 +1000

    save session to disk using save code

--- compd_desktop.py
+++ compd_desktop.py
@@ -78,7 +78,7 @@ def load_saved_data():
 
 
 def set_sidebar_elements():
-    vers_num = '2026-01-27 1844'
+    vers_num = '2026-01-27 1851'
     st.sidebar.image('./logo/compd_logo_white.png',)
     if st.sidebar.button('Clear Data'):
         reset_session_state_params_data()
