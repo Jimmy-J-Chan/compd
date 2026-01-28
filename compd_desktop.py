8c7a8e41d770fd2073c9ef40ea9348570608b098
J <jimmychan0507@gmail.com>
2026-01-28 14:13:02 +1000

    add trade pct to trade analyser, update vers_num

--- compd_desktop.py
+++ compd_desktop.py
@@ -85,7 +85,7 @@ def load_saved_data():
 
 
 def set_sidebar_elements():
-    vers_num = '2026-01-27 1916'
+    vers_num = '2026-01-28 1412'
     st.sidebar.image('./logo/compd_logo_white.png',)
     if st.sidebar.button('Clear Data'):
         reset_session_state_params_data()
