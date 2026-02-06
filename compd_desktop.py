cfdba393a83b3eb341cebe289da86ad819a68330
J <jimmychan0507@gmail.com>
2026-02-06 16:55:41 +1000

    rm outliers

--- compd_desktop.py
+++ compd_desktop.py
@@ -85,7 +85,7 @@ def load_saved_data():
 
 
 def set_sidebar_elements():
-    vers_num = '2026-02-06 1455'
+    vers_num = '2026-02-06 1655'
     st.sidebar.image('./logo/compd_logo_white.png',)
     if st.sidebar.button('Clear Data'):
         reset_session_state_params_data()
